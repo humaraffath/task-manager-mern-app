a40da8da3e5d18d008cf615a99c327fb0133c66e
humaraffath <humaraffath@gmail.com>
2026-04-29 10:11:08 +0530

    Initial Commit

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,18 @@
+import axiosClient from "./axiosClient";
+
+export const registerApi = async ({ name, email, password }) => {
+  const res = await axiosClient.post("/auth/register", {
+    name,
+    email,
+    password,
+  });
+  return res.data; //success,message,data - name,email,role
+};
+
+export const loginApi = async ({ email, password }) => {
+  const res = await axiosClient.post("/auth/login", {
+    email,
+    password,
+  });
+  return res.data; //success,message,data - token,user
+};
---
+++ frontend/src/api/axiosClient.js
@@ -0,0 +1,32 @@
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+
+const axiosClient = axios.create({
+  baseURL,
+  headers: { "Content-Type": "application/json" },
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const auth = getAuth();
+    const token = auth?.token; //error
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(errror),
+);
+
+axiosClient.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const status = error?.response?.status;
+    if (status == 401) {
+      clearAuth();
+      window.location.assign("\login");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
