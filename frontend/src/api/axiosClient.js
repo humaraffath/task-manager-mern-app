3ae7f98e7f802d6f97fb9fba77cea1960a0b8426
humaraffath <humaraffath@gmail.com>
2026-05-04 14:37:47 +0530

    changes

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { getAuth } from "../utils/storage";
+
 const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 const axiosClient = axios.create({
